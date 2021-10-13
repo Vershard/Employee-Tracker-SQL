624ac29b8bb778f7a546d9618589c1c9a72cdae8
Vershard <vershardthomas@gmail.com>
2021-10-13 00:23:01 -0400

    schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -20,14 +20,11 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT UNSIGNED NOT NULL PRIMARY KEY, 
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
     first_name VARCHAR(30) UNIQUE NOT NULL,
     last_name VARCHAR(30) UNIQUE NOT NULL,
     role_id INT UNSIGNED NOT NULL,
-    manager_id INT UNSIGNED,
     FOREIGN KEY (role_id)
     REFERENCES role(id) ON DELETE CASCADE
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id) ON DELETE SET NULL
 );
 
