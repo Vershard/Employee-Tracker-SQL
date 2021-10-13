c69c663158f7fffdd5ba955ed3f4b5389b0b8d12
Vershard <vershardthomas@gmail.com>
2021-10-13 00:22:26 -0400

    index.js

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
-const connection = require("./connection");
+const connection = require("./connection"); 
+
 
 class DB {
     constructor(connection) {
@@ -8,7 +9,7 @@ class DB {
 
     getAllEmps() {
         return this.connection.promise().query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+            "SELECT * FROM employee;"
         )
     }
 
@@ -20,7 +21,7 @@ class DB {
 
     findAllDepartment() {
         return this.connection.promise().query(
-           "SELECT department.id, department.name FROM department;"
+           "SELECT * FROM department;"
         )
     }
 
@@ -33,11 +34,30 @@ class DB {
     }
 
     createEmployee(employee){
+        console.log("DATABASE EMPloyee", employee)
         return this.connection.promise().query(
             "INSERT INTO employee SET ?", employee
         )
     }
 
+    addRole(role){
+        return this.connection.promise().query(
+            'INSERT INTO role SET ?', role
+        )
+    }
+
+    addDepartment(department){
+        return this.connection.promise().query(
+            'INSERT INTO department SET ?', department
+        )
+    }
+
+    updateEmployeeRole(employeeId, roleId){
+        return this.connection.promise().query(
+            "UPDATE employee SET role_id = ? WHERE id = ?",
+        [roleId, employeeId]
+        )
+    }
 }
 
 
